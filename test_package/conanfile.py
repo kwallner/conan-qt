e29c99cbdf6884125e2c6fa127487e00c3b37b13
Karl Wallner <karl.wallner@gmx.de>
2017-12-28 22:32:39 +0100

    Builds on Linux (Ubuntu16.04-64Bit) now.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -4,17 +4,17 @@ from conans import ConanFile, CMake
 
 # This easily allows to copy the package in other user or channel
 CHANNEL = os.getenv("CONAN_CHANNEL", "testing")
-USERNAME = os.getenv("CONAN_USERNAME", "osechet")
+USERNAME = os.getenv("CONAN_USERNAME", "kwallner")
 
 class QtTestConan(ConanFile):
     """ Qt Conan package test """
 
-    requires = "Qt/5.8.0@%s/%s" % (USERNAME, CHANNEL)
+    requires = "Qt/5.9.3@%s/%s" % (USERNAME, CHANNEL)
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "virtualenv"
 
     def build(self):
-        cmake = CMake(self.settings)
+        cmake = CMake(self)
         self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
